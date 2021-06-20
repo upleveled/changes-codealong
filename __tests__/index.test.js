477b96868176ae9c1969cb74402bb355fb545b59
Karl Horky <karl.horky@gmail.com>
2021-06-20 19:40:29 +0200

    Add starting point for codealong

---
+++ __tests__/index.test.js
@@ -0,0 +1,52 @@
+import {
+  aggregateAllChannels,
+  cleanse,
+  deduplicate,
+  filterToCreatedAfter2010,
+  filterToFirstNameStartingWithB,
+  getInterestedRepeatCustomers,
+  getTotalValueOfAllCustomers,
+  getUsersWithNonMatchingEmails,
+  sortByFirstName,
+  sortBySignupDate,
+} from '../index';
+
+test('sorts by first name', () => {
+  expect(sortByFirstName()).toMatchSnapshot();
+});
+
+test('sorts by signup date', () => {
+  expect(sortBySignupDate()).toMatchSnapshot();
+});
+
+test('filters to first name starting with B', () => {
+  expect(filterToFirstNameStartingWithB()).toMatchSnapshot();
+});
+
+test('filters to created after 2010', () => {
+  expect(filterToCreatedAfter2010()).toMatchSnapshot();
+});
+
+test('aggregates data across all channels', () => {
+  expect(aggregateAllChannels()).toMatchSnapshot();
+});
+
+test('deduplicates users', () => {
+  expect(deduplicate()).toMatchSnapshot();
+});
+
+test('cleanses user data', () => {
+  expect(cleanse()).toMatchSnapshot();
+});
+
+test('gets interested, repeat customers', () => {
+  expect(getInterestedRepeatCustomers()).toMatchSnapshot();
+});
+
+test('gets total value of all customers', () => {
+  expect(getTotalValueOfAllCustomers()).toMatchSnapshot();
+});
+
+test('gets users with non-matching emails', () => {
+  expect(getUsersWithNonMatchingEmails()).toMatchSnapshot();
+});
