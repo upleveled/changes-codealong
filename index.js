54583bc52bf2cfbfe96da68ec13c160b3daa313e
Karl Horky <karl.horky@gmail.com>
2021-06-20 19:40:29 +0200

    Add starting point for codealong

---
+++ index.js
@@ -0,0 +1,125 @@
+import csv from 'csv/lib/sync.js';
+import fs from 'fs';
+import {
+  aggregateAllChannels as aggregateAllChannelsTransform,
+  cleanse as cleanseTransform,
+  deduplicate as deduplicateTransform,
+  filterToCreatedAfter2010 as filterToCreatedAfter2010Transform,
+  filterToFirstNameStartingWithB as filterToFirstNameStartingWithBTransform,
+  getInterestedRepeatCustomers as getInterestedRepeatCustomersTransform,
+  getTotalValueOfAllCustomers as getTotalValueOfAllCustomersTransform,
+  getUsersWithNonMatchingEmails as getUsersWithNonMatchingEmailsTransform,
+  sortByFirstName as sortByFirstNameTransform,
+  sortBySignupDate as sortBySignupDateTransform,
+} from './transformations.js';
+
+function parseCsv(inFile) {
+  const fileContent = fs.readFileSync('dataIn/' + inFile, 'utf8');
+  return csv.parse(fileContent, { columns: true });
+}
+
+const inFiles = {
+  hubspot: parseCsv('hubspot-export.csv'),
+  mailchimp: parseCsv('mailchimp-export.csv'),
+  stripe: parseCsv('stripe-export.csv'),
+};
+
+export function sortByFirstName() {
+  return {
+    outFile: 'stripe-sorted-first-name.csv',
+    data: sortByFirstNameTransform(inFiles.stripe),
+  };
+}
+
+export function sortBySignupDate() {
+  return {
+    outFile: 'stripe-sorted-signup-date.csv',
+    data: sortBySignupDateTransform(inFiles.stripe),
+  };
+}
+
+export function filterToFirstNameStartingWithB() {
+  return {
+    outFile: 'hubspot-first-name-b.csv',
+    data: filterToFirstNameStartingWithBTransform(inFiles.hubspot),
+  };
+}
+
+export function filterToCreatedAfter2010() {
+  return {
+    outFile: 'stripe-created-after-2010.csv',
+    data: filterToCreatedAfter2010Transform(inFiles.stripe),
+  };
+}
+
+export function aggregateAllChannels() {
+  return {
+    outFile: 'all-channels-aggregate.csv',
+    data: aggregateAllChannelsTransform(
+      inFiles.hubspot,
+      inFiles.mailchimp,
+      inFiles.stripe,
+    ),
+  };
+}
+
+export function deduplicate() {
+  return {
+    outFile: 'stripe-deduplicated.csv',
+    data: deduplicateTransform(inFiles.stripe),
+  };
+}
+
+export function cleanse() {
+  return {
+    outFile: 'hubspot-cleansed.csv',
+    data: cleanseTransform(inFiles.hubspot),
+  };
+}
+
+export function getInterestedRepeatCustomers() {
+  return {
+    outFile: 'all-channels-interested-repeat-customers.csv',
+    data: getInterestedRepeatCustomersTransform(
+      inFiles.hubspot,
+      inFiles.stripe,
+    ),
+  };
+}
+
+export function getTotalValueOfAllCustomers() {
+  return {
+    outFile: 'hubspot-total-value-all-customers.csv',
+    data: getTotalValueOfAllCustomersTransform(inFiles.hubspot),
+  };
+}
+
+export function getUsersWithNonMatchingEmails() {
+  return {
+    outFile: 'all-channels-users-non-matching-emails.csv',
+    data: getUsersWithNonMatchingEmailsTransform(
+      inFiles.mailchimp,
+      inFiles.stripe,
+    ),
+  };
+}
+
+const commandFn = {
+  sortByFirstName,
+  sortBySignupDate,
+  filterToFirstNameStartingWithB,
+  filterToCreatedAfter2010,
+  aggregateAllChannels,
+  deduplicate,
+  cleanse,
+  getInterestedRepeatCustomers,
+  getTotalValueOfAllCustomers,
+  getUsersWithNonMatchingEmails,
+}[process.argv[2]];
+
+if (commandFn) {
+  const { outFile, data } = commandFn();
+  const outFilePath = 'dataOut/' + outFile;
+  fs.writeFileSync(outFilePath, csv.stringify(data, { header: true }));
+  console.log(`Wrote file ${outFilePath} successfully!`);
+}
---
+++ transformations.js
@@ -0,0 +1,46 @@
+// Codealong: Data Sorting and Filtering in Node.js
+export function sortBySignupDate(input) {
+  return input;
+}
+
+export function sortByFirstName(input) {
+  return input;
+}
+
+export function filterToFirstNameStartingWithB(input) {
+  return input;
+}
+
+export function filterToCreatedAfter2010(input) {
+  return input;
+}
+
+// Codealong: Data Aggregation, Deduplication and Cleansing in Node.js
+export function aggregateAllChannels(
+  inputHubSpot,
+  inputMailchimp,
+  inputStripe,
+) {
+  return [];
+}
+
+export function deduplicate(input) {
+  return input;
+}
+
+export function cleanse(input) {
+  return input;
+}
+
+// Codealong: Data Analysis in Node.js
+export function getInterestedRepeatCustomers(inputHubspot, inputStripe) {
+  return [];
+}
+
+export function getTotalValueOfAllCustomers(input) {
+  return input;
+}
+
+export function getUsersWithNonMatchingEmails(inputStripe, inputMailchimp) {
+  return [];
+}
