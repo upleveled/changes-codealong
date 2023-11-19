e63d6604b7d7eab213070b7b154bfc2d30b69f61
Karl Horky <karl.horky@gmail.com>
2023-11-19 22:47:40 +0100

    Add back disabled rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,13 @@
-export { default } from 'eslint-config-upleveled';
+import upleveled from 'eslint-config-upleveled';
+
+/** @type {import('@typescript-eslint/utils/ts-eslint').FlatConfig.ConfigArray} */
+const config = [
+  ...upleveled,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+];
+
+export default config;
